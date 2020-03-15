4b24e71ac1c1987491b5ce3390c78910b163840f
Salavin <zeldatwili1@gmail.com>
2020-03-15 12:05:54 -0500

    Added (?) icon and fixed some small things.

--- js/javascript.js
+++ js/javascript.js
@@ -3,26 +3,3 @@ $(".nav-item").click(function() {
         scrollTop: $("#nav-tabContent").offset().top -56
     }, 500);
 });
-
-// function moveScroller() {
-//     var $anchor = $("#background");
-//     var $scroller = $('#sticky');
-//
-//     var move = function() {
-//         var st = $(window).scrollTop();
-//         var ot = $anchor.offset().top;
-//         if(st > ot) {
-//             $scroller.css({
-//                 position: "fixed",
-//                 top: "0px"
-//             });
-//         } else {
-//             $scroller.css({
-//                 position: "relative",
-//                 top: ""
-//             });
-//         }
-//     };
-//     $(window).scroll(move);
-//     move();
-// }
